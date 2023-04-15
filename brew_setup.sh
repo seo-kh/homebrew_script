8e5ff6899b1f81a445367fe5d592f5a7babd2432
서광현 (seo-kh) <gospelseokh@gmail.com>
2023-04-15 17:14:24 +0900

    first brew_setup sh

---
+++ brew_setup.sh
@@ -0,0 +1,42 @@
+# Install Homebrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# Git
+brew install git 
+brew install git-lfs
+git config --global user.name "seo-kh
+git config --global user.email "gospelseokh@gmail.com"
+git config --global core.precomposeunicode true
+git config --global core.quotepath false
+
+# iOS App Development
+brew install --cask sf-symbols
+brew install vapor 
+
+
+# android App Development
+brew install --cask android-studio
+
+# Flutter
+brew install flutter
+
+# Web Browsers
+brew install --cask google-chrome
+brew install --cask brave-browser
+
+# Figma
+brew install --cask figma
+
+# Notion
+brew install --cask notion
+
+# Development Tools
+brew install --cask github
+brew install --cask visual-studio-code
+brew install --cask postman
+
+# Utils
+brew install --cask spectacle
+brew install --cask discord
+brew install --cask zoom
+brew install --cask slack
